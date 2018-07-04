60aef1e34fddbdc5323b7745690ac9f54f9c25a5
QingDengYue <zrl412@163.com>
2018-07-04 11:36:22 +0800

    Node Version 8,9

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular6-ssr-boilerplate!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app');
   }));
 });
