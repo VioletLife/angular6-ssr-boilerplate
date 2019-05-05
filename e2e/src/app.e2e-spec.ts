79a505c2ad1897667a1f224e2399ad15220bb980
ZhaoRuiLong <zrl412@163.com>
2019-05-05 18:58:09 +0800

    update Angular 7

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -3,10 +3,12 @@ import { AppPage } from './app.po';
 describe('workspace-project App', () => {
   let page: AppPage;
 
+  // @ts-ignore
   beforeEach(() => {
     page = new AppPage();
   });
 
+  // @ts-ignore
   it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to angular6-ssr-boilerplate!');
